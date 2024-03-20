e4fc12437f033cea1f80cc1e50eb829ad02891f2
Lucas <lucas.mariano1991@outlook.com>
2024-03-19 23:22:48 -0300

    alterado nome da pasta e nova lista lp03

---
+++ src/lp03/Primos.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lp03;
+
+/**
+ *
+ * @author skynn
+ */
+public class Primos {
+    
+    int num;
+    int count = 1;
+    
+    
+    public void setNum(int userNum) {
+        num = userNum;
+        
+    }    
+    
+    public Primos() {
+        num = 0;
+        
+    }
+    
+    public int checkPrimo() {
+        for (int i = 1; i <= num; i++) {
+            if ()
+        }
+    } 
+}
