d1f15f0ca41c5e7ed8e2be5fa78341ce6690d3b9
Lucas <lucas.mariano1991@outlook.com>
2024-03-22 00:17:16 -0300

    ex05

--- src/lp03/EX05.java
+++ src/lp03/EX05.java
@@ -4,10 +4,37 @@
  */
 package lp03;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author skynn
  */
 public class EX05 {
-    
+    public static void main(String[] args) {
+        
+        Incrementar inc = new Incrementar(0,0);
+        
+        
+        try {
+            String inputLimite = JOptionPane.showInputDialog(null, "Digite o limite: ");
+            String inputIncremento = JOptionPane.showInputDialog(null, "Digite o valor do incremento: ");
+
+            int limite, incremento;
+
+            limite = Integer.parseInt(inputLimite);
+            incremento = Integer.parseInt(inputIncremento);
+
+            inc.setLimite(limite);
+            inc.setIncremento(incremento);
+
+            inc.funcIncremento(inc.getLimite(), inc.getIncremento());
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Não foi inserido um número");
+        }
+        
+        
+        
+    }
 }
---
+++ src/lp03/Incrementar.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lp03;
+
+/**
+ *
+ * @author skynn
+ */
+public class Incrementar {
+    
+    private int limite;
+    private int incremento;
+
+    public Incrementar(int limite, int incremento) {
+        this.limite = limite;
+        this.incremento = incremento;
+    }
+
+    public int getLimite() {
+        return limite;
+    }
+
+    public void setLimite(int limite) {
+        this.limite = limite;
+    }
+
+    public int getIncremento() {
+        return incremento;
+    }
+
+    public void setIncremento(int incremento) {
+        this.incremento = incremento;
+    }
+    
+    public void funcIncremento(int limite, int incremento) {        
+            
+        for (int i = 0; i <= limite; i += incremento) {
+            System.out.println(i);
+        }
+    }   
+    
+}
